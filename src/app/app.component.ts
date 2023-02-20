3887813b1e1b9f4f3bf4dc95e1f8ad6cc19076be
Balaji <balaji.vishnubhotla@scadea.com>
2023-02-20 18:09:04 +0530

    Added login form to test hb-input primitive of text and password types;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-npm-library';
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl(false)
+  });
+  pageType?: string;
+  showPassword: boolean = true;
+
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+  
 }
